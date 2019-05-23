cc05a7ae4f57db307fd748c98e1564388f80e993
P822025 <42806244+p822025@users.noreply.github.com>
2019-05-23 22:02:28 +0800

    test cws

--- cws.py
+++ cws.py
@@ -1,6 +1,23 @@
 import requests
 import json
 
-req = requests.get('https://github.com/timeline.json')
+serpd='http://10.97.235.18:9080'
+serts='http://10.121.11.180:8080'
+
+serurl=serpd
+
+# 自訂表頭
+my_headers = {'accept': 'application/json'}
+
+# 將自訂表頭加入 GET 請求中
+req = requests.get(serpd +"/NPIWS/service/CAS/WARD/TEST/A102",headers = my_headers)
 doc=json.loads(req.text)
-print (doc['message'])
+retcode= doc['outMsg']['ret']
+if retcode==0:
+    print('has data!!')
+    for vo in doc['outMsg']['rescont']:
+        hisid=vo['hisid']
+        print(hisid)
+    
+else:
+    print('has error!!!')
